d4ef2da6dc7984515630d813df3601ea661444fc
Lars Bilke <lars.bilke@ufz.de>
2019-03-28 15:58:52 +0100

    Fixed path to cli.py.

--- ogscm/build_common_definitions.py
+++ ogscm/build_common_definitions.py
@@ -5,13 +5,13 @@ subprocess.run(f"python build.py --pm system --cvode "
                "--ompi off 2.1.2 3.1.2 4.0.0",
                shell=True)
 
-subprocess.run(f"python build.py --file Dockerfile.gcc.full --jenkins "
+subprocess.run(f"python cli.py --file Dockerfile.gcc.full --jenkins "
                "--cppcheck --docs --gcovr",
                shell=True)
-subprocess.run(f"python build.py --file Dockerfile.gcc.gui --jenkins "
+subprocess.run(f"python cli.py --file Dockerfile.gcc.gui --jenkins "
                "--cppcheck --gui --gcovr",
                shell=True)
-subprocess.run(f"python build.py --file Dockerfile.clang.full "
+subprocess.run(f"python cli.py --file Dockerfile.clang.full "
                "--base_image ubuntu:18.04 --compiler 7 --clang --jenkins "
                "--iwyy",
                shell=True)
