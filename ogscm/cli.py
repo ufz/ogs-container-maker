2bdb2024c170fc30d6634b7261d2309be822488a
Lars Bilke <lars.bilke@ufz.de>
2019-08-27 09:46:17 +0200

    Moved cli args to own implementation file.

--- ogscm/cli.py
+++ ogscm/cli.py
@@ -27,104 +27,15 @@ from hpccm.primitives import baseimage, comment, user, environment, raw, \
     label, shell, copy
 
 import ogscm
+from ogscm.cli_args import Cli_Args
 from ogscm.config import package_manager
 from ogscm.version import __version__
 from ogscm.building_blocks import ccache, cppcheck, cvode, eigen, iwyy, \
     jenkins_node, ogs_base, ogs, osu_benchmarks, petsc, pm_conan, vtk, pm_spack
 
 
-def main(): # pragma: no cover
-    cli = argparse.ArgumentParser(
-        description='Generate container image definitions.',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    cli.add_argument('--version', action='version',
-                     version='%(prog)s {version}'.format(version=__version__))
-    cli.add_argument('--out', type=str, default='_out', help='Output directory')
-    cli.add_argument('--file', type=str, default='', help='Overwrite output recipe file name')
-    cli.add_argument('--print', '-P', dest='print', action='store_true',
-                     help='Print the definition to stdout')
-    options_g = cli.add_argument_group('Combinatorial options',
-                                       'All combinations of the given options will '
-                                       'be generated')
-    options_g.add_argument('--format', nargs='*', type=str,
-                           choices=['docker', 'singularity'],
-                           default=['docker'])
-    options_g.add_argument('--pm', nargs='*', type=str,
-                           choices=['system', 'conan', 'spack', 'off'], default=['conan'],
-                           help='Package manager to install third-party '
-                                'dependencies')
-    options_g.add_argument('--ompi', nargs='*', type=str,
-                           default=['off'],
-                           help='OpenMPI version, e.g. 2.1.1, 2.1.5, 3.0.1, 3.1.2')
-    options_g.add_argument('--ogs', nargs='*', type=str,
-                           default=['ufz/ogs@master'],
-                           help='OGS GitHub repo in the form \'user/repo@branch\' '
-                                'or \'off\' to disable OGS building')
-    options_g.add_argument('--cmake_args', nargs='*', type=str, default=[''],
-                           help='CMake argument sets have to be quoted and **must**'
-                                ' start with a space. e.g. --cmake_args \' -DFIRST='
-                                'TRUE -DFOO=BAR\' \' -DSECOND=TRUE\'')
-    build_g = cli.add_argument_group('Image build options')
-    build_g.add_argument('--build', '-B', dest='build', action='store_true',
-                         help='Build the images from the definition files')
-    build_g.add_argument('--upload', '-U', dest='upload', action='store_true',
-                         help='Upload Docker image to registry')
-    build_g.add_argument('--registry', type=str,
-                         default='registry.opengeosys.org/ogs/ogs',
-                         help='The docker registry the image is tagged and '
-                              'uploaded to.')
-    build_g.add_argument('--convert', '-C', dest='convert', action='store_true',
-                         help='Convert Docker image to Singularity image')
-    build_g.add_argument('--runtime-only', '-R', dest='runtime_only',
-                         action='store_true',
-                         help='Generate multi-stage Dockerfiles for small runtime '
-                              'images')
-    switches_g = cli.add_argument_group('Additional options')
-    switches_g.add_argument('--base_image', type=str, default='ubuntu:18.04',
-                            help='The base image. \'centos:7\' is supported too.')
-    switches_g.add_argument('--compiler', type=str, default='gcc',
-                            help='The compiler to use. Possible options: off, gcc, clang')
-    switches_g.add_argument('--compiler_version', type=str, default='',
-                            help='Compiler version.')
-    switches_g.add_argument('--gui', dest='gui', action='store_true',
-                            help='Builds the GUI (Data Explorer)')
-    switches_g.add_argument('--docs', dest='docs', action='store_true',
-                            help='Setup documentation requirements (Doxygen)')
-    switches_g.add_argument('--jenkins', dest='jenkins', action='store_true',
-                            help='Setup Jenkins slave')
-    switches_g.add_argument('--cvode', dest='cvode', action='store_true',
-                            help='Install and configure with cvode')
-    switches_g.add_argument('--cppcheck', dest='cppcheck', action='store_true',
-                            help='Install cppcheck')
-    switches_g.add_argument('--iwyy', dest='iwyy', action='store_true',
-                            help='Install include-what-you-use')
-    switches_g.add_argument('--gcovr', dest='gcovr', action='store_true',
-                            help='Install gcovr')
-    switches_g.add_argument('--mpi_benchmarks', dest='mpi_benchmarks',
-                            action='store_true', help='Installs OSU MPI '
-                            'benchmarks as scif app and mpi_bw, mpi_ring,'
-                            'mpi_hello')
-    switches_g.add_argument('--dev', dest='dev', action='store_true',
-                            help='Installs development tools (vim, gdb)')
-    maint_g = cli.add_argument_group('Maintenance')
-    maint_g.add_argument('--clean', dest='cleanup', action='store_true',
-                         help='Cleans up generated files in default directories.')
-
-    cli.set_defaults(build=False)
-    cli.set_defaults(convert=False)
-    cli.set_defaults(print=False)
-    cli.set_defaults(runtime_only=False)
-    cli.set_defaults(upload=False)
-    cli.set_defaults(gui=False)
-    cli.set_defaults(docs=False)
-    cli.set_defaults(jenkins=False)
-    cli.set_defaults(cvode=False)
-    cli.set_defaults(cppcheck=False)
-    cli.set_defaults(iwyy=False)
-    cli.set_defaults(gcovr=False)
-    cli.set_defaults(mpi_benchmarks=False)
-    cli.set_defaults(dev=False)
-    cli.set_defaults(cleanup=False)
+def main():  # pragma: no cover
+    cli = Cli_Args()
     args = cli.parse_args()
 
     if args.jenkins:
