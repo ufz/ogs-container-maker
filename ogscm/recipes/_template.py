f905ffef9364ac84b571a3f38fb6476e3cc32dfd
Lars Bilke <lars.bilke@ufz.de>
2021-07-13 14:40:25 +0200

    Update recipe template.

--- ogscm/recipes/_template.py
+++ ogscm/recipes/_template.py
@@ -5,8 +5,6 @@
 from hpccm.primitives import comment
 from hpccm.building_blocks import xx
 
-print(f"Evaluating {filename}")
-
 # Add cli arguments to args_parser
 parse_g = parser.add_argument_group(filename)
 
@@ -16,15 +14,16 @@ parse_g.add_argument("--my_arg", type=str, default="default_value")
 # Parse local args
 local_args = parser.parse_known_args()[0]
 
-### SET append to image file name, e.g.:
-img_file += f"-someName-{local_args.my_arg}"
+if execute:
+    ### SET append to image file name, e.g.:
+    img_file += f"-someName-{local_args.my_arg}"
 
-### SET Append to out_dir, e.g.:
-out_dir += f"/someName/{local_args.my_arg}"
+    ### SET Append to out_dir, e.g.:
+    out_dir += f"/someName/{local_args.my_arg}"
 
-# Implement recipe
-Stage0 += comment(f"Begin {filename}")
+    # Implement recipe
+    Stage0 += comment(f"Begin {filename}")
 
-Stage0 += xx(arg="value")
+    Stage0 += xx(arg="value")
 
-Stage0 += comment(f"--- End {filename} ---")
+    Stage0 += comment(f"--- End {filename} ---")
